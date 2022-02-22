36923d96aabd5bf4bb18cc361ffa665b470db07a
Ulugbek01 <xyz207820@gmail.com>
2022-02-23 00:54:01 +0500

    Filter fuction add

--- src/components/Buyurtmalar/index.jsx
+++ src/components/Buyurtmalar/index.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import Header from '../Header';
-import { Container } from './style';
+import React,{useState} from 'react'
+import { Container, HeaderContainer, AddProductWrapper, IconWrapper, FilterItemWrapper, FilterItem, ToggleBtnWrapper } from './style';
+import {ReactComponent as Plus} from '../../assets/icons/plus.svg';
+import {ReactComponent as IconX} from '../../assets/icons/Group 2.svg';
+import {ReactComponent as IconY} from '../../assets/icons/x.svg';
 
 export const Buyurtmalar = () => {
+  const [active, setActive] = useState('Yangi');
+  const [isToggle, setIsToggle] = useState(true);
+
+  const onActive = (text)=> {
+    setActive(text);
+  }
+
+  const Toggle = () => {
+    setIsToggle(!isToggle);
+    console.log(isToggle);
+  }
   return (
-    <>
-      <Header title="buyurtma"/>
-        <Container>
-          Buyurtmalar
-        </Container>
-    </>
+    <div>
+      <HeaderContainer>
+        <AddProductWrapper>
+          <IconWrapper>
+            <Plus />
+          </IconWrapper>
+          <p className='title'>Yangi buyurtma<br /> qo'shish</p>
+        </AddProductWrapper>
+
+        <FilterItemWrapper>
+          <div className='filter-items__wrapper'>
+            <FilterItem active={active==='Yangi'} onClick={()=>onActive('Yangi')}>Yangi</FilterItem>
+            <FilterItem active={active==='Qabul qilingan'} onClick={()=>onActive('Qabul qilingan')}>Qabul qilingan</FilterItem>
+            <FilterItem active={active==="Jo'natilgan"} onClick={()=>onActive("Jo'natilgan")}>Jo'natilgan</FilterItem>
+            <FilterItem active={active==='Yopilgan'} onClick={()=>onActive('Yopilgan')}>Yopilgan</FilterItem>
+          </div>
+        </FilterItemWrapper>
+
+        <ToggleBtnWrapper>
+          <div className='container'>
+            <div className='icon-wrapper' onClick={Toggle}><IconX/></div>
+            <div className='icon-wrapper' onClick={Toggle}><IconX/></div>
+          </div>
+        </ToggleBtnWrapper>
+      </HeaderContainer>
+      <Container>
+        Buyurtmalar
+      </Container>
+    </div>
   )
 }
 
--- src/components/Buyurtmalar/style.js
+++ src/components/Buyurtmalar/style.js
@@ -1,5 +1,99 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const display_flex = css`
+  display: flex;
+  align-items: center;
+`;
 
 export const Container = styled.div`
   display: flex;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+`;
+
+export const AddProductWrapper = styled.div`
+  ${display_flex};
+  height: 80px;
+  width: 240px;
+  background-color: #fff;
+  padding: 0 20px;
+  border-left: 2px solid #e5e5e5;
+  border-right: 2px solid #e5e5e5;
+
+  .title {
+    font-weight: 600;
+    font-size: 14px;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  ${display_flex};
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  background-color: #20d472;
+  border-radius: 50%;
+  cursor: pointer;
+  margin-right: 14px;
+`;
+
+export const FilterItemWrapper = styled.div`
+  height: 80px;
+  background-color: #fff;
+  padding: 16px 24px;
+
+  .filter-items__wrapper {
+    ${display_flex};
+    height: 48px;
+    background-color: #edeff3;
+    border-radius: 24px;
+    padding: 0 5px;
+
+    & > div {
+      text-align: center;
+      line-height: 36px;
+    }
+  }
+`;
+
+export const FilterItem = styled.div`
+  width: 160px;
+  height: 36px;
+  background-color: ${({ active }) => active && "#fff"};
+  box-shadow: ${({ active }) =>
+    active ? "0px 2px 2px rgba(174, 176, 181, 0.314986)" : "none"};
+  border-radius: 18px;
+  cursor: pointer;
+`;
+
+export const ToggleBtnWrapper = styled.div`
+  ${display_flex};
+  justify-content: center;
+  flex: 1;
+  height: 80px;
+  background-color: #fff;
+  border-left: 2px solid #e5e5e5;
+
+  .container {
+    ${display_flex};
+    justify-content: space-between;
+    width: 100px;
+    height: 48px;
+    background-color: #edeff3;
+    border-radius: 24px;
+    padding: 6px;
+  }
+
+  .icon-wrapper {
+    width: 36px;
+    height: 36px;
+    line-height: 36px;
+    background-color: #fff;
+    box-shadow: 0px 2px 2px rgba(174, 176, 181, 0.314986);
+    border-radius: 18px;
+    text-align: center;
+    cursor: pointer;
+  }
 `;
