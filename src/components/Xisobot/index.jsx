55feb79f5a59a6ee5ebdf2adc00d0f57e83532e4
Ulugbek01 <xyz207820@gmail.com>
2022-03-01 01:31:34 +0500

    victoryChart add

--- src/components/Xisobot/index.jsx
+++ src/components/Xisobot/index.jsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import Header from '../Header';
 import { Container } from './style';
+import {VictoryBar, VictoryChart, VictoryLine} from 'victory';
+
+const data = [
+  {quarter: 'Feb 13'},
+  {quarter: 'Feb 14'},
+  {quarter: 'Feb 15'},
+  {quarter: 'Feb 16'},
+  {quarter: 'Feb 17'},
+  {quarter: 'Feb 18'},
+];
+
+const sampleData = [
+  {x: 1.0, y: 2.0},
+  {x: 2.0, y: 3.0},
+  {x: 3.0, y: -4.0},
+  {x: 4.0, y: 5.0},
+  {x: 5.0, y: 6.0},
+  { y: 7.0}
+]
 
 export const Xisobot = () => {
   return (
-    <div>
+    <>
       <Header title='xisobot'/>
-      <Container>Xisobot</Container>
-    </div>
+      <Container>
+        <Container.Item>
+          <Container.Item.Header>
+            <div className='title'>Yopilgan zakazlar | Barcha filial</div>
+            <div className='deta'>13.02.2020 - 19.02.2020</div>
+          </Container.Item.Header>
+          <VictoryChart>
+            <VictoryBar
+              data={data}
+              x="quarter"
+            />
+          </VictoryChart>
+        </Container.Item>
+        <Container.Item>
+        <Container.Item.Header>
+          <div className='title'>Atmen zakazlar | Barcha filial</div>
+          <div className='deta'>13.02.2020 - 19.02.2020</div>
+        </Container.Item.Header>
+        <VictoryChart>
+            <VictoryLine data={sampleData} />
+        </VictoryChart>
+        </Container.Item>
+      </Container>
+    </>
   )
 }
 
--- src/components/Xisobot/style.js
+++ src/components/Xisobot/style.js
@@ -2,4 +2,28 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  align-items: center;
+  margin: 100px 0 20px;
+  padding: 0 40px;
+`;
+
+Container.Item = styled.div`
+  background-color: #fff;
+  box-shadow: 0px 2px 2px rgba(174, 176, 181, 0.314986);
+  border-radius: 6px;
+  flex: 1;
+
+  &:first-child {
+    margin-right: 10px;
+  }
+`;
+
+Container.Item.Header = styled.div`
+  height: 50px;
+  background-color: #fff;
+  box-shadow: 0px 2px 6px rgba(174, 176, 181, 0.314986);
+  border-radius: 6px 6px 0px 0px;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
 `;
